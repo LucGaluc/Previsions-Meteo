4c7c79e914bac66fecf70cf011fbfd5974cfaf65
LucGaluc <amartinoluc@gmail.com>
2023-10-10 09:44:37 +0200

    recherche fonctionne

--- src/WheatherApp/WheatherApp.jsx
+++ src/WheatherApp/WheatherApp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import WeatherForecast from "../WeatherForecast/WheatherForecast";
+import WeatherForecast from "../WeatherForecast/WheatherForecast.jsx";
 import "./WheatherApp.css";
 
 const API_KEY = "d8fdde2b90b925ccfcfeb01ea0401eed";
@@ -10,8 +10,10 @@ const WeatherApp = () => {
     const [weatherData, setWeatherData] = useState([]);
     // indique si les données sont en cours de chargement
     const [loading, setLoading] = useState(true);
-    // Utilisation de la géolocalisation
+    // Utilisation géolocalisation
     const [useGeolocation, setUseGeolocation] = useState(false);
+    // stocker la valeur de la ville
+    const [city, setCity] = useState("");
 
     // Obtenir les données météo avec géoloc
     const getWeatherDataByLocation = async () => {
@@ -22,17 +24,17 @@ const WeatherApp = () => {
                 navigator.geolocation.getCurrentPosition(async (position) => {
                     // API avec les coordonnées de latitude et de longitude
                     const apiUrl = `${API_URL}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`;
-                    // Requête HTTP GET avec fetch
+                    // GET avec fetch
                     const response = await fetch(apiUrl);
                     // on attend JSON parse
                     const data = await response.json();
-                    // Mise à jour données météorologiques
+                    // Mise à jour données météo
                     setWeatherData(data.list);
-                    // chargement est terminé
+                    // chargement terminé
                     setLoading(false);
                 });
             } else {
-                // Alerte si la géolocalisation n'est pas prise en charge
+                // Alerte si loc n'est pas prise en charge
                 alert(
                     "La géolocalisation n'est pas prise en charge par votre navigateur."
                 );
@@ -56,7 +58,7 @@ const WeatherApp = () => {
             // réponse en JSON parse et on attend qu'elle arrive
             const data = await response.json();
 
-            // Mise à jour avec les données météorologiques
+            // Mise à jour avec données météo
             setWeatherData(data.list);
             // chargement terminé
             setLoading(false);
@@ -68,44 +70,40 @@ const WeatherApp = () => {
         }
     };
 
-    // UseEffect pour charger les données initiales en fonction de la géolocalisation
+    // UseEffect pour charger les données initiales en fonction géoloc
     useEffect(() => {
         if (useGeolocation) {
-            // Si la géolocalisation est activée
+            // géoloc activée
             getWeatherDataByLocation();
         } else {
-            // Sinon, Données de la ville par défaut (Cannes)
+            // Données de la ville par défaut (Cannes)
             getWeatherData("Cannes");
         }
     }, [useGeolocation]);
 
+    // bouton "Rechercher"
+    const handleSearch = () => {
+        // il faut que la ville ne soit pas vide
+        if (city.trim() !== "") {
+            getWeatherData(city);
+        }
+    };
+
     return (
         <div className="weather-app">
             <h1>Bulletin Météo</h1>
-            {/* <h5 >(avec Cannes par defaut)</h5>*/}
             <div>
                 {/* activer la géolocalisation */}
                 <button onClick={() => setUseGeolocation(true)}>
                     Utiliser la Géolocalisation
                 </button>
-                <div>
-                    <button onClick={() => setUseGeolocation(false)}>
-                        <input
-                            placeholder="Entrez votre ville"
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                        />
-                        Rechercher votre Ville
-                    </button>
-                    <br />
-                    <button onClick={handleSearch}>Rechercher</button>
-                </div>
+                <button onClick={handleSearch}>Rechercher</button>
             </div>
             {loading ? (
                 <p>Chargement en cours...</p>
             ) : (
                 <div className="weather-forecast">
-                    {/* Affichez les prévisions météorologiques à partir des données récupérées */}
+                    {/* Prévisions météorologiques à partir des données récupérées */}
                     {weatherData.map((forecast) => (
                         <WeatherForecast
                             key={forecast.dt}
